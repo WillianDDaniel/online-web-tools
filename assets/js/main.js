7a326a8a0b5bb545976f0237a055042dc38dbc0f
Willian Daniel <williandeivitidaniel@live.com>
2025-05-02 14:42:22 -0300

    feat: Update footer copyright year dynamically and enhance footer link styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
 document.getElementById('toggleThemeButton').addEventListener('click', function () {
   this.classList.toggle('toggle-theme-button-checked');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('year').textContent = new Date().getFullYear();
+});
