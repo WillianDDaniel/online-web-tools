984616e59796ad752ec3a6842bc17002cbefca71
WillianDDaniel <williandeivitidaniel@live.com>
2026-05-04 11:10:24 -0300

    Refactor and update JavaScript files for text comparison and conversion tools; remove deprecated files and update HTML references to new asset versions.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-document.getElementById('toggleThemeButton').addEventListener('click', function () {
-  this.classList.toggle('toggle-theme-button-checked');
-});
-
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('year').textContent = new Date().getFullYear();
 });
